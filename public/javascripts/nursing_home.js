25fe2782fec23ce15adc42040a6d7d1aebea111a
malachytobin <malachytobin@gmail.com>
2015-06-23 19:38:34 -0500

    Added a call to medicare socrata web service, converted to geojson, mapped

--- public/javascripts/nursing_home.js
+++ public/javascripts/nursing_home.js
@@ -1,17 +1,28 @@
 var census_sdk_key = '4ef6c883a1c176cab8018f754fe64ee4d95df38e';
 
-function callGeoSearch(){
-     var geoJson = new L.geoJson(null, {onEachFeature:onEachNursingHome});
+function address_search_click(){
+  
+  callGeoSearch(-87.666214, 42.019814);
+}
+
+function callGeoSearch(longitude, latitude){
+      var base_url = "https://data.medicare.gov/resource/4pq5-n9py.json";
+      var within_qry = "?$where=within_circle(location,42.019814,-87.666214,1000)";
+      var geoJson = new L.geoJson(null, {onEachFeature:onEachNursingHome});
       geoJson.addTo(Window.map);
 
       $.ajax({
          dataType: "json",
-          url: "sample_nh.json",
+          url: base_url + within_qry,
           success: function(data) {
-            $(data.features).each(function(key, data) {
+            var geojson_features = convertJsonToGeoJson(data)
+            $(geojson_features.features).each(function(key, data) {
             geoJson.addData(data);
             });
-            Window.map.fitBounds(geoJson.getBounds());
+            if(geoJson.getBounds()!=null){
+              Window.map.fitBounds(geoJson.getBounds());  
+            }
+            
           }
       }).error(function() {});
 }
@@ -42,9 +53,38 @@ function callCensusGeocode(){
             output.append("Population: " + response.data[0].population + " </br>");
             output.append("<br/><br/><br/>Here's the address object we sent and what matched:<br/><br/>");
             output.append("Address: " + JSON.stringify(response.address, null, 4) + "<br/>");
+        callGeoSearch(response.lng , response.lat);
     });
 }
 
+  function convertJsonToGeoJson(features){
+    var geoJsonArr =[];
+      for(var feat in features){
+        var geoJsonfeat = {};
+        geoJsonfeat.type = "Feature";
+        geoJsonfeat.geometry = createPointGeom(features[feat].location.longitude, features[feat].location.latitude);
+        var props = {};
+        props.provnum = features[feat].federal_provider_number;
+        props.provname = features[feat].provider_name;
+        props.address = features[feat].provider_address;
+        geoJsonfeat.properties = props;
+        geoJsonArr.push(geoJsonfeat);
+        // "provnum":"145126","provname":"ALDEN LINCOLN REHAB & H C CTR","address":"504 WEST WELLINGTON AVENUE"
+      }
+      var geoJson = {};
+      geoJson.type = "FeatureCollection";
+      geoJson.features = geoJsonArr;
+      return geoJson
+  }
+
+function createPointGeom(longitude, latitude){
+  // {"type":"Point", "coordinates": [-87.642136, 41.936554]}
+  var geom = {};
+  geom.type="Point";
+  geom.coordinates= [longitude, latitude];
+
+  return geom;
+}
 function onEachNursingHome(feature, layer){
   
     var list = "<dl><dt>Id:</dt>"
