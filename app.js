6e8dd9b9c286224abb64aa6b376a096dd96d382b
pjsier <pjsier@gmail.com>
2015-07-02 14:42:59 -0500

    another proxy attempt

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('./config/localhost.key').toString();
+var certificate = fs.readFileSync('./config/localhost.crt').toString();
+var options = {
+  key : privateKey
+, cert : certificate
+}
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,15 +18,41 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// Start server.
+var port = process.env.PORT || 3000; // Used by Heroku and http on localhost
+process.env['PORT'] = process.env.PORT || 4000; // Used by https on localhost
 
-var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
- };
+http.createServer(app).listen(port, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
- app.use(forceSsl);
+// Run separate https server if on localhost
+if (process.env.NODE_ENV != 'production') {
+    https.createServer(options, app).listen(process.env.PORT, function () {
+        console.log("Express server listening with https on port %d in %s mode", this.address().port, app.settings.env);
+    });
+};
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(function (req, res, next) {
+        res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+        if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+            return res.redirect(301, 'https://' + req.host + req.url);
+        } else {
+            return next();
+            }
+    });
+} else {
+    app.use(function (req, res, next) {
+        res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+        if (!req.secure) {
+            return res.redirect(301, 'https://' + req.host  + ":" + process.env.PORT + req.url);
+        } else {
+            return next();
+            }
+    });
+
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
